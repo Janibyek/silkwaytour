c2255242461955879878238ca85a3252a642a09b
Avidal Janibyek <83421830+Janibyek@users.noreply.github.com>
2025-02-09 23:39:30 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,3 +30,73 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('nextBtn').addEventListener('click', nextSlide);
     document.getElementById('prevBtn').addEventListener('click', prevSlide);
 });
+let slideIndex1 = 0;
+showSlides1();
+
+function showSlides1() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides1");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex1++;
+    if (slideIndex1 > slides.length) {slideIndex1 = 1}
+    slides[slideIndex1 - 1].style.display = "block";
+    setTimeout(showSlides1, 3000); // 2 секунд тутамд зураг солигдоно
+}
+let slideIndex2 = 0;
+showSlides2();
+
+function showSlides2() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides2");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex2++;
+    if (slideIndex2 > slides.length) {slideIndex2 = 1}
+    slides[slideIndex2 - 1].style.display = "block";
+    setTimeout(showSlides2, 3000); // 2 секунд тутамд зураг солигдоно
+}
+let slideIndex3 = 0;
+showSlides3();
+
+function showSlides3() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides3");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex2++;
+    if (slideIndex2 > slides.length) {slideIndex2 = 1}
+    slides[slideIndex2 - 1].style.display = "block";
+    setTimeout(showSlides3, 3000); // 2 секунд тутамд зураг солигдоно
+}
+let slideIndex4 = 0;
+showSlides4();
+
+function showSlides4() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides4");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex2++;
+    if (slideIndex2 > slides.length) {slideIndex2 = 1}
+    slides[slideIndex2 - 1].style.display = "block";
+    setTimeout(showSlides4, 3000); // 2 секунд тутамд зураг солигдоно
+}
+let slideIndex5 = 0;
+showSlides5();
+
+function showSlides5() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides5");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex2++;
+    if (slideIndex2 > slides.length) {slideIndex2 = 1}
+    slides[slideIndex2 - 1].style.display = "block";
+    setTimeout(showSlides5, 3000); // 2 секунд тутамд зураг солигдоно
+}
